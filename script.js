c5600a005cf8856b9897d0cfa4adbcebfd67a120
AndreiIonica <40264929+AndreiIonica@users.noreply.github.com>
2021-06-01 17:25:10 +0300

    autogenerate table

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-$(document).ready(function () {
-   // alert('ceva');
-   
-});
+document.addEventListener('DOMContentLoaded', onReady, false);
+
+const NUM_ROWS = 10;
+
+function onReady() {
+  const table = document.querySelector('.table > tbody');
+
+  for (let i = 0; i < NUM_ROWS; i++) {
+    table.appendChild(createRow());
+  }
+}
+
+function createRow() {
+  const row = document.createElement('tr');
+
+  for (let i = 0; i < NUM_ROWS; i++) {
+    let cell = document.createElement('td');
+    row.appendChild(cell);
+  }
+
+  return row;
+}
